f02d6f4437a319c9966834da0024614325c9a604
Linad <merzlikindaniil97@gmail.com>
2023-09-24 19:56:10 +0300

    done program function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Итоговая контрольная работа");
+
+string[] begin = {"Hello", "2", "world", ":-)"};
+returnArr(begin, 3);
+
+
+void returnArr(string[] begin, int sizeWord)
+{
+    int indexBeginArr = 0;
+    int indexEndArr = 0;
+    string[] result = new string[indexEndArr];
+    string[] buf = new string[indexEndArr];
+    while (indexBeginArr<begin.Length)
+    {
+        if (begin[indexBeginArr].Length<=sizeWord)
+        {
+            indexEndArr++;
+            buf = new string[indexEndArr];
+            for (int i = 0; i<indexEndArr-1; i++)
+            {
+                buf[i] = result[i];
+            }
+            buf[indexEndArr - 1] = begin[indexBeginArr];
+            result = buf;
+        }
+        indexBeginArr++;
+    }
+    for (int i = 0; i < indexEndArr; i++)
+    {
+        if (i==indexEndArr-1)
+        {
+            Console.Write(result[i]);
+        }
+        else
+        {
+            Console.Write(result[i] + ", ");
+        }
+        
+    }
+}
